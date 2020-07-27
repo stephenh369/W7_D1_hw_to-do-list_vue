d7204196c492f888c4c1560265154077bf540204
Stephen <stephen_h369@live.co.uk>
2020-07-27 16:13:15 +0100

    added app.js code to save new tasks, some html to display tasks

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
-        
+        data: {
+            tasks: [],
+            newTask: ''
+        },
+        methods: {
+            saveNewTask: function() {
+                this.tasks.push({
+                    name: this.newTask,
+                    isComplete: false
+                });
+                this.newTask = '';
+            }
+        }
     });
 });
